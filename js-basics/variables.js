5e8bdafcbc0b17410e964c890e05367f318648b2
Ankit082000 <ankitrathore082000@gmail.com>
2025-10-29 06:33:27 +0000

    container config done

---
+++ js-basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;                               // integer ke liye const ka use karenge
+let accountEmail = "ankitrathore867@gmail.com";       // String keyword ke liye hum let ka use karenge 
+var accountPassword = "12345";                       // var ka nahi kyuki var ka scope pure code pe hota hai
+accountCity = "Noida";
+
+accountEmail="ankit082000@gmail.com";
+accountPassword="082000";
+accountCity="Gurugram";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
