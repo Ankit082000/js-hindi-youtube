011d0199c1e6d178814c999209504236d673553c
Ankit082000 <ankitrathore082000@gmail.com>
2025-10-29 13:32:04 +0000

    variables

--- js-basics/variables.js
+++ js-basics/variables.js
@@ -7,4 +7,19 @@ accountEmail="ankit082000@gmail.com";
 accountPassword="082000";
 accountCity="Gurugram";
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// console.log(3+3+"2");
+
+let idx=4;
+idx++;
+// console.log(idx);
+
+// console.log("5">4);             //isne apne aap hi ek string ko number mai convert kar diya hai
+// console.log(5>"4");            // jabki aisa nhi hona chahiye tha
+
+console.log("2"===2);            // isme ye data type bhi check karega 
+console.log("2"==2);            // isme sirf value check hogi aur isne string ko number mai convert kar diya ha
+
+// primitivie type mai (Stack memory use hoti hai)
+//Non primitive type mai (Heap memory use hoti hai)
